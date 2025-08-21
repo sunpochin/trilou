012dcda638adf95e72f3e6d1dffaf89ec10422f0
sunpochin <sunpochin@gmail.com>
2025-08-21 14:08:57 +0800

    remove .output

--- .output/server/chunks/build/SkeletonLoader-styles.CJm9Fec8.mjs
+++
@@ -1,6 +0,0 @@
-import { S as SkeletonLoader_vue_vue_type_style_index_0_scoped_9079f87b_lang } from './entry-styles-2.mjs-B8zRl9d3.mjs';
-
-const SkeletonLoaderStyles_CJm9Fec8 = [SkeletonLoader_vue_vue_type_style_index_0_scoped_9079f87b_lang];
-
-export { SkeletonLoaderStyles_CJm9Fec8 as default };
-//# sourceMappingURL=SkeletonLoader-styles.CJm9Fec8.mjs.map
